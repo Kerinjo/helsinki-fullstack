8518cdbd7f0d3eff3b7b1583b5ffaf68a5273d60
Konrad Marciniak <konmarciniak@gmail.com>
2022-12-12 23:26:20 +0100

    notes on event handlers

--- part1/lectures/lecture-c/src/ReRenderingApp.js
+++ part1/lectures/lecture-c/src/ReRenderingApp.js
@@ -2,19 +2,27 @@ import { useState } from 'react'
 
 const App = () => {
     const [ counter, setCounter ] = useState(0)
-    
-    setTimeout(
-        () => setCounter(counter + 1),
-        1000
-    )
+        
+    const increaseByOne = () => setCounter(counter + 1)
+    const setToZero = () => setCounter(0)
+
     // When the state modyfing function setCounter is called,
     // React re-renders the component which means, that the
     // function body gets re-executed.
-
-    console.log('rendering...', counter)
-
+    
     return (
+        <>
         <div>{counter}</div>
+        {/* Usually defining event handlers within JSX-templates
+        is not a good idea. Here, the value of onClick attribute
+        is a variable containing a reference to the function */}
+        <button onClick={increaseByOne}>
+            plus
+        </button>
+        <button onClick={setToZero}>
+            zero
+        </button>
+        </>
     )
 }
 
