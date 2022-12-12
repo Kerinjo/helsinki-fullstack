fa56140c5550f67805f5a81636335ab9ed9654b6
Konrad Marciniak <konmarciniak@gmail.com>
2022-12-12 01:00:17 +0100

    notes on Component state and destructuring

---
+++ part1/lectures/lecture-c/src/ReRenderingApp.js
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+const App = () => {
+    const [ counter, setCounter ] = useState(0)
+    
+    setTimeout(
+        () => setCounter(counter + 1),
+        1000
+    )
+    // When the state modyfing function setCounter is called,
+    // React re-renders the component which means, that the
+    // function body gets re-executed.
+
+    console.log('rendering...', counter)
+
+    return (
+        <div>{counter}</div>
+    )
+}
+
+export default App
