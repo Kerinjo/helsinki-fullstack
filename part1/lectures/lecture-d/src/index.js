f09a563ccb4989ae280bee960a628a859cb342d0
Konrad Marciniak <konmarciniak@gmail.com>
2022-12-17 23:40:43 +0100

    Various ways of defining event handlers, finished lecture D

--- part1/lectures/lecture-d/src/index.js
+++ part1/lectures/lecture-d/src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import EventApp from './EventApp';
+import FunctionApp from './FunctionApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('EventApp')).render(<EventApp />)
+// ReactDOM.createRoot(document.getElementById('EventApp')).render(<FunctionApp />)
