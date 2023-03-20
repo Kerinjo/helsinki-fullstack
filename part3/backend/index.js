5d99d774655f24fcd24871c91a0c66e785f61bcc
Kerinjo <konmarciniak@gmail.com>
2023-03-20 19:37:32 +0100

    Added POST and DELETE handling, added REST Client requests for debug

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,6 +1,7 @@
-const e = require('express')
 const express = require('express')
 const app = express()
+
+app.use(express.json())
 
 let notes = [
   {
@@ -35,8 +36,44 @@ app.get('/api/notes/:id', (request, response) => {
   if (note) {
     response.json(note)
   } else {
+    // 404 not found
     response.status(404).end()
   }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+  // 204 no content
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    // 400 bad request
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId()
+  }
+  
+  notes = notes.concat(note)
+  
+  response.json(note)
 })
 
 const PORT = 3001
